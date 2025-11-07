5103ba659e8417edec7d32828249eea23b9a048e
Daiana Poza <daianapoza@gmail.com>
2025-11-07 19:09:37 -0300

    build listo para deploy en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repo = "invitacion-boda-dai-diego";
+
+
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: `/${repo}/`,      // importante para que carguen los assets en Pages
+  build: { outDir: "docs" } // generamos el build en /docs
 });
